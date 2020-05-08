4cd88cfa52a3dd5b3a51441342413ad0d516f0aa
cyeongb <46704590+cyeongb@users.noreply.github.com>
2020-05-08 09:24:47 +0900

    Update TodoList.js
    주석 추가

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -2,6 +2,11 @@ import React from "react";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.scss";
 
+/* TodoList
+  todos배열을 props로 받아온 후 
+  내장 배열함수 map을 사용해서 여러개의 TodoListItem 컴포넌트로 변환해서 보여준다.
+*/
+
 const TodoList = ({ todos, onRemove, onToggle }) => {
   return (
     <div className="TodoList">
