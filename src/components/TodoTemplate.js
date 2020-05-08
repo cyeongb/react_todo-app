973b38be8378d0a72fb9b97e029d3917524267f2
cyeongb <46704590+cyeongb@users.noreply.github.com>
2020-05-08 09:19:38 +0900

    Update TodoTemplate.js
    주석추가

--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,6 +1,11 @@
 import React, { Fragment } from "react";
 import "./TodoTemplate.scss";
 
+/* TodoTemplate 
+ 화면을 가운데로 정렬시켜 주고 , 일정 관리를 보여준다.
+ children으로 내부 JSX를 props로 받아와서 렌더링 해 준다.
+*/
+
 const TodoTemplate = ({ children }) => {
   return (
     <div className="TodoTemplate">
